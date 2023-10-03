934aae4224d5697772b3b2b05ab03b5709b889d4
phucnguyen117 <144077716+phucnguyen117@users.noreply.github.com>
2023-10-03 09:21:54 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,32 @@
+package basic.dev;
+/**
+ * Ghi Chú (comment nhiều dòng)
+ * @author HoangHuuPhucNguyen
+ * 1. Tên lớp trùng tên flie
+ * 2. Tên lớp chữ cái đầu viết hoa. Nếu từ ghép thì những chữ cái đầu viết hoa 
+ * 4. Tên lớp đặt phải có nghĩa. Ví dụ: Class Peson : lớp mô tả con người 
+ * - Interface:	Phải có tiền tố I. Ví dụ: IListener 
+ *  - Abstract: Phải có tiền tố Abstract. Ví dụ: AbstractShape
+ */
+public class MainApp {
+	// 23CDTH41
+	/* 23CDTH41*/
+	public static void main (String[] args) {
+		System.out.println("Hello World");
+		/**
+		 * Cách đặt tên biến
+		 * 1. Không chứa ký tự đặc biệt !@#$%^&*()-+=
+		 * 2. Không chứa khoảng trắng
+		 * 3. Viết thường
+		 * 4. Không được bắt đầu bắt đầu bằng số 
+		 */
+		int number;
+		String nameSchool;		
+	}
+	/*
+	 * tên phương thức tuân theo quy tắc camelCase
+	 */
+	public void show() {
+		
+	}	
+}
